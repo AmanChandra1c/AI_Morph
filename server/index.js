1d656bb5081cf5c53a41b596e2bf8bd9fab5a485
AMAN CHANDRAVANSHI <146625429+AmanChandra1c@users.noreply.github.com>
2025-10-29 21:17:31 +0530

    Extend session cookie maxAge to one week

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ app.use(
     saveUninitialized: false,
     store,
     cookie: {
-      maxAge: 1000 * 60 * 15,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
       httpOnly: true,
       secure: false,
     },
