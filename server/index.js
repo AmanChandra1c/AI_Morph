9b5bc6858316bdd4ba505e1ae29838283d25c2e2
aman <amanchandra1c7662@gmail.com>
2025-10-29 15:55:41 +0530

    fix: Correct formatting and update CORS origin for production environment

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ import cors from "cors";
 import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js";
 import imgGenerateRoutes from "./routes/imgGenerateRoutes.js";
-import createUserRoutes from "./routes/createUserRoutes.js"
+import createUserRoutes from "./routes/createUserRoutes.js";
 import logInUserRoutes from "./routes/logInUserRoutes.js";
-import getUser from "./routes/getUser.js"
-import getPost from "./routes/getPost.js"
+import getUser from "./routes/getUser.js";
+import getPost from "./routes/getPost.js";
 import profile from "./routes/profile.js";
 import sendOTP from "./routes/sendOTP.js";
 import cookieParser from "cookie-parser";
@@ -26,13 +26,13 @@ setSessionStore(store);
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "https://ai-morph-rdn9.onrender.com",
     credentials: true,
   })
 );
 app.use(
   session({
-    secret: process.env.JWT_SECRET  ,
+    secret: process.env.JWT_SECRET,
     resave: false,
     saveUninitialized: false,
     store,
@@ -45,31 +45,28 @@ app.use(
 );
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({limit: "50mb", extended: true}))
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/imgGenerate", imgGenerateRoutes);
 app.use("/api/v1/create-user", createUserRoutes);
 app.use("/api/v1/login-user", logInUserRoutes);
-app.use("/api/v1/get-user", getUser)
-app.use("/api/v1/get-post", getPost)
+app.use("/api/v1/get-user", getUser);
+app.use("/api/v1/get-post", getPost);
 app.use("/api/v1/profile", profile);
 app.use("/api/v1/send-otp", sendOTP);
 
-
 app.get("/", async (req, res) => {
-    res.send("Hello there");
+  res.send("Hello there");
 });
 
 const startServer = async () => {
-    try {
-        connectDB(process.env.MONGODB_URL);
-        app.listen(8000, () =>
-            console.log("Server started")
-        );
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8000, () => console.log("Server started"));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
